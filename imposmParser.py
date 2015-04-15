e0ba7fa3267912c47cabc957b4f85adf2ce06969
Shidan Xu <shidanxu@mit.edu>
2015-04-15 15:25:45 -0400

    updates

--- imposmParser.py
+++ imposmParser.py
@@ -3,7 +3,7 @@ import csv
 
 
 # change here to change the file you want to edit, make sure it is under data/
-filename = "cambridgema"
+filename = "quito"
 
 
 # This is the road conversion, feel free to modify or add
@@ -27,33 +27,39 @@ road_capacity_conversion['service'] = 1
 
 # The ones missing are cycleway and footway (for boston datasets)
 
-
+reference = {}
+plotting_points = []
 ## This method is not called since we parse nodes instead of roads, but can be implemented in the future to count # of stores.
 def parseNodes(nodes):
   print 'hello!\n'
   array = []
-  for id, description, (x, y) in nodes:
-  	print description
+  for id, description, (y, x) in nodes:
+  	# print description
+  	reference[id] = (description, [x, y])
   	if 'addr:housenumber' not in description:
   		array.append(description)
 
 
 # This is the main method to parse roads.
 def parseWays(ways):
-	reference = {}
 	mapping = {}
 	
 	# Each entry in ways is in this format: id, {some description dictionary}, [coordinates]
 	for id, description, coordinates in ways:
+		# print coordinates
 		# They also include some places, for now if an entry has 'highway' and 'name' we know for sure it is a road.
-		if ('highway' in description and 'name' in description):
+		if ('highway' in description):
 			# uncomment this line to see what a description looks like
 			# print description
 			reference[id] = description
 
 			
-			name = "" or description['name']
-			lanes = int(description['lanes']) if 'lanes' in description else ""
+			name = description['name'] if 'name' in description else ""
+			try:
+				lanes = int(description['lanes']) if 'lanes' in description else ""
+			except ValueError:
+				lanes = description['lanes']
+
 			oneway = description['oneway'] if 'oneway' in description else ""
 
 			road_capacity_from_conversion = 1
@@ -70,10 +76,17 @@ def parseWays(ways):
 					# print description['highway']
 			
 			mapping[id] = [name, lanes, road_capacity_from_conversion, highway, oneway]
+
+			for identity in coordinates:
+				if (identity in reference):
+					# print reference[identity]
+					if isinstance(reference[identity], tuple):
+						plotting_points.append(reference[identity][1])
+					else:
+						print reference[identity]
 		else:
 			continue
-			# uncomment this line to see which elements (not roads) are not included in this parsing
-			# print description
+#            print description
 
 
 	# Write to output file when done
@@ -90,6 +103,14 @@ with open ('processed/roads_' + filename+ '.csv', 'wb') as f:
 	header = ["name", "lanes", "road_capacity_from_conversion", "highway", "oneway"]
 	w = writer.writerow(header)
 # Here ways_callback tells us which method we are calling for the ways we found. Change to nodes_callback = yourmethod if want to process stores
-p = XMLParser(ways_callback=parseWays)
+p = XMLParser(ways_callback=parseWays, nodes_callback = parseNodes)
 
 p.parse('data/' + filename)
+p.parse('data/' + filename)
+
+with open ('processed/roads_' + filename + '_coordinates.csv', 'wb') as f:
+	writer = csv.writer(f)
+	for coordinate in plotting_points:
+		writer.writerow(coordinate)
+
+
